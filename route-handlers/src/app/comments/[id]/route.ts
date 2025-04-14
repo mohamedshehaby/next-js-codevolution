3f69aee3ac813812adea0dd89ab2e62184601466
mohamedshehaby <mohamedshehaby365@gmail.com>
2025-04-14 16:51:04 +0200

    add route handlers methods - comments

--- route-handlers/src/app/comments/[id]/route.ts
+++ route-handlers/src/app/comments/[id]/route.ts
@@ -1,6 +1,7 @@
 import { comments } from "@/app/comments/data";
+import { log } from "console";
 export async function GET(
-  request: Request,
+  _request: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
@@ -13,4 +14,41 @@ export async function GET(
   }
 
   return Response.json(comment);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const { text } = await request.json();
+  const comment = comments.find(
+    (comment: { id: number; text: string }) => comment.id === parseInt(id)
+  );
+
+  if (!comment) {
+    return Response.json({ message: "Comment not found" }, { status: 404 });
+  }
+
+  comment.text = text;
+
+  return Response.json(comment);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const comment = comments.find(
+    (comment: { id: number; text: string }) => comment.id === parseInt(id)
+  );
+
+  if (!comment) {
+    return Response.json({ message: "Comment not found" }, { status: 404 });
+  }
+
+  comments.splice(comments.indexOf(comment), 1);
+
+  return Response.json({ message: "Comment deleted" }, { status: 200 });
 }
--- route-handlers/src/app/comments/route.ts
+++ route-handlers/src/app/comments/route.ts
@@ -1,6 +1,16 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get("query");
+
+  if (query) {
+    return Response.json(
+      comments.filter((comment) => comment.text.includes(query))
+    );
+  }
+
   return Response.json(comments);
 }
 
