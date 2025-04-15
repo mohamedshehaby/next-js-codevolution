3773dda571db7d32bb034e2320ed582f240ba392
mohamedshehaby <mohamedshehaby365@gmail.com>
2025-04-15 15:56:19 +0200

    learn headers & cookies

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,28 @@
+import { headers } from "next/headers";
+import { cookies } from "next/headers";
+
 export async function GET() {
-  return Response.json({ message: "Hello, world!" });
+  const headersList = await headers();
+
+  for (const [key, value] of headersList.entries()) {
+    console.log(key, value);
+  }
+  const token = headersList.get("authorization");
+
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("theme");
+
+  console.log("theme -> ", theme);
+
+  return Response.json(
+    {
+      message: "User profile",
+      token,
+    },
+    {
+      headers: {
+        "Set-Cookie": `theme=dark`,
+      },
+    }
+  );
 }
