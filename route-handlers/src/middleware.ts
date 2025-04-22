47bd69409cae567d224dfcb2957739d3b7556653
mohamedshehaby <mohamedshehaby365@gmail.com>
2025-04-22 10:23:40 +0200

    add middleware cookies and headers

--- route-handlers/src/middleware.ts
+++ route-handlers/src/middleware.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 // // This function can be marked `async` if using `await` inside
 // export function middleware(request: NextRequest) {
 //   return NextResponse.redirect(new URL("/", request.url));
@@ -8,15 +10,26 @@
 //   matcher: "/profile",
 // };
 
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// export function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname.startsWith("/profile")) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith("/profile")) {
-    return NextResponse.redirect(new URL("/", request.url));
+//   if (request.nextUrl.pathname.startsWith("/time")) {
+//     return NextResponse.rewrite(new URL("/", request.url));
+//   }
+// }
+
+export default function middleware(request: NextRequest) {
+  const themePreference = request.cookies.get("theme");
+  const response = NextResponse.next();
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
   }
 
-  if (request.nextUrl.pathname.startsWith("/time")) {
-    return NextResponse.rewrite(new URL("/", request.url));
-  }
+  return response;
 }
+
+export const config = {
+  matcher: "/profile",
+};
