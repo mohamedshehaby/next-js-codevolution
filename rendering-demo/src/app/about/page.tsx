949d5f12a5043fa74f13458bae59634778ec87b9
mohamedshehaby <mohamedshehaby365@gmail.com>
2025-05-20 12:27:45 +0300

    add dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function About() {
-  console.log("About");
-  return <div>About</div>;
+import { cookies } from 'next/headers'
+
+export default async  function About() {
+  const cookieStore = await cookies()
+  const theme = cookieStore.get('theme')
+
+  console.log(theme)
+  return <div>About {new Date().toTimeString()} {theme?.name}</div>;
 }
